61781f9461915b13cd31b8bef7e610a089021dcd
code-xzh <xizuhan123@126.com>
2025-04-21 20:33:02 +0800

    更换了窗口布局

--- main.cpp
+++ main.cpp
@@ -1,15 +1,11 @@
-#include "mainwindow.h"
+#include "widget.h"
 
 #include <QApplication>
-#include "model/data.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-    LOG()<<"hello";
-
-    MainWindow w;
+    Widget w;
     w.show();
     return a.exec();
 }
