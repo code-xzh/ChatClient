0a7302d3e80797f65925b488326ea2eb2663dff1
xi-zuhan <xizuhan123@126.com>
2025-05-16 22:00:47 +0800

    数据中心 类定义核心数据结构

--- datacenter.cpp
+++ datacenter.cpp
@@ -1,5 +1,34 @@
 #include "datacenter.h"
 
-DataCenter::DataCenter(QObject *parent)
-    : QObject{parent}
-{}
+namespace model {
+
+DataCenter *DataCenter::_dataCenter=nullptr;
+
+DataCenter *DataCenter::getInstance()
+{
+    if(_dataCenter==nullptr)
+        _dataCenter=new DataCenter();
+    return _dataCenter;
+}
+
+DataCenter::DataCenter()
+{
+    _memberList=new QHash<QString,QList<UserInfo>>();
+    _recentMessages=new QHash<QString,QList<Message>>();
+    _unreadMessagesCount=new QHash<QString,int>();
+}
+
+DataCenter::~DataCenter()
+{
+    delete _myself;
+    delete _friendList;
+    delete _chatSessionList;
+    delete _memberList;
+    delete _applyList;
+    delete _recentMessages;
+    delete _unreadMessagesCount;
+    delete _searchUserResult;
+    delete _searchMessageResult;
+}
+
+}//end model
--- datacenter.h
+++ datacenter.h
@@ -2,14 +2,40 @@
 #define DATACENTER_H
 
 #include <QWidget>
+#include "model/data.h"
+
+namespace model {
 
 class DataCenter : public QObject
 {
     Q_OBJECT
 public:
-    explicit DataCenter(QObject *parent = nullptr);
+    static DataCenter* getInstance();
+
+private:
+    DataCenter();
+    ~DataCenter();
+
+    static DataCenter*  _dataCenter;
+
+private:
+    //要管理的数据
+    QString _loginSessionId="";//登录会话id
+    UserInfo* _myself=nullptr;//当前用户信息
+    QList<UserInfo>* _friendList=nullptr;//好友列表,指针更容易表示无效值
+    QList<ChatSessionInfo>* _chatSessionList=nullptr;//会话列表
+    QString _currentChatSessionId="";//记录当前选中的会话
+    QHash<QString,QList<UserInfo>>* _memberList=nullptr;//记录会话中有哪些成员(主要针对群聊),key:chatSessionId,value:成员列表
+    QList<UserInfo>* _applyList=nullptr;//待处理的好友申请列表
+    QHash<QString,QList<Message>>* _recentMessages=nullptr;//每个会话最近消息列表,key:chatSessionId,value:消息列表
+    QHash<QString,int>* _unreadMessagesCount=nullptr;//每个会话未读消息个数,key:chatSessionId,value:未读消息个数
+    QList<UserInfo>* _searchUserResult=nullptr;//用户的好友搜索结果
+    QList<Message>* _searchMessageResult=nullptr;//保存历史消息搜索结果
+    QString _currentVertifyCodeId;//短信验证码的验证id
 
 signals:
 };
+
+}//end model
 
 #endif // DATACENTER_H
